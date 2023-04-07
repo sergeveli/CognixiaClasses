a9c0af735163228497f94a4dac5508ad3a70b9eb
sergeveli <70241892+sergeveli@users.noreply.github.com>
2023-04-07 00:35:58 -0400

    TicTacToe still janky. Will revisit.

--- W1D4/gameStation/menu/tic_tac_toe.py
+++ W1D4/gameStation/menu/tic_tac_toe.py
@@ -20,26 +20,34 @@ def display_board(board):
     print(f"  {board[6]}  |  {board[7]}  |  {board[8]}")
     print("     |     |")
 
-def check_game_over(board):
-    """Define a function to check if the game is over"""
-    # Check rows
-    for i in range(0, 9, 3):
-        if board[i : i + 3] in [["X", "X", "X"], ["O", "O", "O"]]:
-            return True
-    return next(
-        (
-            True
-            for i in range(3)
-            if board[i::3] in [["X", "X", "X"], ["O", "O", "O"]]
-        ),
-        True
-        if board[0] == board[4] == board[8] or board[2] == board[4] == board[6]
-        else " " not in board,
-    )
+# Define a function to check the state of the game
 
+
+def check_game_over(board):
+    """Check rows"""
+    for i in range(0, 9, 3):
+        if board[i:i+3] == ["X", "X", "X"]:
+            return "X wins"
+        elif board[i:i+3] == ["O", "O", "O"]:
+            return "O wins"
+    # Check columns
+    for i in range(3):
+        if board[i::3] == ["X", "X", "X"]:
+            return "X wins"
+        elif board[i::3] == ["O", "O", "O"]:
+            return "O wins"
+    # Check diagonals
+    if board[0] == board[4] == board[8]:
+        return f"{board[0]} wins"
+    elif board[2] == board[4] == board[6]:
+        return f"{board[2]} wins"
+    # Check for a tie
+    return "Tie" if " " not in board else "Playing"
+
+
+# Define a function to play the game
 def play_game():
-    """Define a function to play the game"""
-    # Load the game progress from the log file
+    """Load the game progress from the log file"""
     progress = {"wins": 0, "losses": 0, "ties": 0}
     # Play the game until the user decides to quit
     while True:
@@ -58,9 +66,12 @@ def play_game():
         if user_option.upper() == "X":
             user_option = "X"
             computer_option = "O"
-        else:
+        elif user_option.upper() == "O":
             user_option = "O"
             computer_option = "X"
+        else:
+            print("Invalid option. Please choose X or O.")
+            continue
         # Display the board
         display_board(board)
         # Play the game until it is over
@@ -70,7 +81,14 @@ def play_game():
                 "Enter the number of an empty space to make a move (or q to quit): ")
             if user_move.lower() == "q":
                 break
-            user_move = int(user_move) - 1
+            try:
+                user_move = int(user_move) - 1
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and 9.")
+                continue
+            if user_move < 0 or user_move > 8:
+                print("Invalid input. Please enter a number between 1 and 9.")
+                continue
             # Make the user's move
             if board[user_move] == " ":
                 board[user_move] = user_option
@@ -98,4 +116,5 @@ def play_game():
             progress["losses"] += 1
         else:
             print("Invalid game state!")
+    # Return the game progress
     return progress
