a23a30476c5f72ed6d24618c3e72134c25047df3
sergeveli <70241892+sergeveli@users.noreply.github.com>
2023-04-11 12:57:24 -0400

    fixed up. all functional.

--- W2D2/employee_exercise.py
+++ W2D2/employee_exercise.py
@@ -1,85 +1,99 @@
-"""This program prompts the user for the number of employees they want to add, 
-then loops through each employee and prompts for their name, age, and birth year. 
-It formats the name, generates the email, and saves the data to a file. 
-Finally, it prints each employee's information in a formatted string."""
+"""Here's a program that captures user input for an Employee, 
+stores the data in a list of dictionaries, and saves it to a file. 
+It also includes a module for handling integer user input 
+and a main script that tests the functions in the module. 
+This program uses list comprehension to get a list of employees under 30, 
+and it uses filter and map to get a list of emails for employees over 30."""
 
-import datetime
-import os
+def get_int_input(prompt, within_range=None):
+    """
+    Prompts the user to enter an integer value and returns it.
 
-def get_input(prompt, data_type=str, min_value=None, max_value=None):
-    """Define a function to get user input with error handling"""
+    Args:
+        prompt (str): The prompt to display to the user.
+        min_val (int, optional): The minimum allowed value. Defaults to None.
+        max_val (int, optional): The maximum allowed value. Defaults to None.
+
+    Returns:
+        int: The integer value entered by the user.
+
+    Raises:
+        ValueError: If user does not enter a valid int value or if the value is outside the range.
+    """
     while True:
         try:
-            user_input = data_type(input(prompt))
-            if min_value is not None and user_input < min_value:
-                raise ValueError(f"Input must be at least {min_value}")
-            if max_value is not None and user_input > max_value:
-                raise ValueError(f"Input must be at most {max_value}")
-            return user_input
-        except ValueError as error:
-            print(f"Invalid input: {error}")
-
-def format_name(name):
-    """Define a function to format the name"""
-    first_name, last_name = name.split()
-    first_name = first_name.capitalize()
-    last_name = last_name.capitalize()
-    return f"{first_name} {last_name}"
-
-def generate_email(name, birth_year):
-    """Define a function to generate the email"""
-    first_name, last_name = name.split()
-    last_name += str(birth_year)[-2:]
-    return f"{first_name.lower()}.{last_name.lower()}@company.com"
-
-# Define the main function to capture employee data
+            value = int(input(prompt))
+            if within_range is not None and (value < within_range[0] or value > within_range[1]):
+                raise ValueError(
+                    f"Value must be between {within_range[0]} and {within_range[1]}")
+            return value
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
 
 def main():
-    """Get the number of employees"""
-    num_employees = get_input(
-        "How many employees will you add? ", int, min_value=1, max_value=10)
+    """
+    A function that prompts the user to input employee information, creates a list of employees,
+    and writes the employee data to a file. The function utilizes the `get_int_input` function from
+    the `input_handler` module to handle user input and error handling. It also uses list comp
+    and the `filter` and `map` functions to create a separate list of employees categorized by age.
 
-    # Create a list to hold employee dictionaries
-    employees = []
+    Parameters:
+        None
 
-    # Loop for each employee and record their information
-    for i in range(num_employees):
-        # Get the employee's name and split it into first and last
-        name = get_input(f"Enter the name of employee #{i+1}: ")
-        name = format_name(name)
+    Returns:
+        None
+    """
+    emps = []
+    max_emps = 5
 
-        # Get the employee's age and birth year
-        birth_year = get_input(
-            f"Enter the birth year of employee #{i+1}: ", int, min_value=1900, 
-            max_value=datetime.datetime.now().year)
-        age = get_input(
-            f"Enter the age of employee #{i+1}: ", int, min_value=0)
+    while True:
+        num_emps = get_int_input(
+            "How many employees would you like to add? ", within_range=(1, max_emps))
+        if len(emps) + num_emps <= max_emps:
+            break
+        else:
+            print(
+                f"Error: Only {max_emps - len(emps)} employees can be added.")
 
-        # Generate the email
-        email = generate_email(name, birth_year)
+    for i in range(num_emps):
+        print(f"Employee {i+1}")
+        first_name = input("Enter first name: ").capitalize()
+        last_name = input("Enter last name: ").capitalize()
+        age = get_int_input("Enter age: ")
+        birth_year = get_int_input("Enter birth year: ")
+        email = f"{first_name.lower()}.{last_name.lower()}{str(birth_year)[-2:]}@company.com"
+        emp = {
+            "first": first_name,
+            "last": last_name,
+            "age": age,
+            "birth_year": birth_year,
+            "email": email
+        }
+        emps.append(emp)
 
-        # Add the employee to the list
-        employee = {"Name": name, "Age": age,
-                    "Birth Year": birth_year, "Email": email}
-        employees.append(employee)
+    with open("employees.txt", "a", encoding="UTF-8") as _f:
+        for _ in emps:
+            _f.write(
+                f"{_['first']} {_['last']} {_['age']} {_['birth_year']} {_['email']}\n")
 
-    # Print each employee's information in a formatted string
-    for employee in employees:
+    print("Employees:")
+    for _ in emps:
         print(
-            f"Name: {employee['Name']}\nAge: {employee['Age']}\nBirth Year: {employee['Birth Year']}\nEmail: {employee['Email']}\n")
+            f"{_['first']} {_['last']}, age {_['age']}, email: {_['email']}")
 
-    # Save the employee data to a file
-    filename = "employee_data.txt"
-    if not os.path.exists(filename):
-        with open(filename, "w", encoding="UTF-8") as file:
-            file.write("Name,Age,Birth Year,Email\n")
-    with open(filename, "a", encoding="UTF-8") as file:
-        for employee in employees:
-            file.write(
-                f"{employee['Name']},{employee['Age']},{employee['Birth Year']},{employee['Email']}\n")
+    # Use list comprehension to get employees under 30
+    young_emps = [
+        emp for emp in emps if emp['age'] < 30]
+    print("Employees Under 30:")
+    for _ in young_emps:
+        print(f"{_['first']} {_['last']}")
 
+    # Use filter and map to get emails for employees over 30
+    old_emp_emails = list(map(lambda emp: emp['email'], filter(lambda emp: emp['age'] >= 30, emps)))
+    print("Emails for Employees Over 30:")
+    for email in old_emp_emails:
+        print(email)
 
-# Call the main function
 if __name__ == "__main__":
     main()
